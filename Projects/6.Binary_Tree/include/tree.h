471c0efa459ed5868415a7e2b3bc09e3982ab4b8
Shishqa <jp190429@gmail.com>
2019-11-09 00:12:34 +0300

    tree

--- Projects/6.Binary_Tree/include/tree.h
+++ Projects/6.Binary_Tree/include/tree.h
@@ -1,8 +1,38 @@
 template <typename elem_t>
+struct Node
+{
+    elem_t data;
+    Node<elem_t> * left;
+    Node<elem_t> * right;
+
+    bool init (elem_t data);
+    bool clear ();
+
+    Node<elem_t> * addLeft (elem_t data);
+    Node<elem_t> * addRight (elem_t data);
+
+    bool deleteList ();
+    bool deleteSubtree ();
+};
+
+
+template <typename elem_t>
 class BinaryTree
 {
-    public:
-        
+private:
+    size_t size;
 
-    private:
+public:
+    Node<elem_t> * root;
+
+    bool init (elem_t data);
+    bool clear ();
+
+    Node<elem_t> * search (elem_t data);
+
+    bool valid ();
+
+    bool print (FILE* log);
+    bool dump (FILE* log);
+    bool dotDump ();
 };
